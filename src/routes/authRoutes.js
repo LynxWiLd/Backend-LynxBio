0d0519c72fac6ea0276dfdcd06a79fdcee3c7880
Vera_Facundo <facundovera647@gmail.com>
2026-03-13 23:54:06 -0300

    feature: api para agregar avatar

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { upload } from '../config/cloudinary.js';
 // Agregamos todas las funciones que faltaban al import
 import {
   register,
@@ -16,5 +17,16 @@ router.post("/login", login);
 router.get("/me", auth, getMe);
 router.get("/profile/:username", getPublicProfile);
 router.put("/settings", auth, updateSettings);
+router.post('/upload-avatar', auth, upload.single('image'), async (req, res) => {
+  try {
+    const user = await User.findById(req.userId);
+    user.profile.avatarUrl = req.file.path; // La URL que nos da Cloudinary
+    await user.save();
+    res.json({ url: req.file.path });
+  } catch (err) {
+    res.status(500).send('Error al subir imagen');
+  }
+});
+
 
 export default router;
