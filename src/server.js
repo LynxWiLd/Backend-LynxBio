238289b6bcc68690dd163bea4da4d4c20c5dea8c
Vera_Facundo <facundovera647@gmail.com>
2026-03-14 14:29:33 -0300

    relase: cambio la ruta

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js"; 
+import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import linkRoutes from "./routes/linkRoutes.js";
 
@@ -15,7 +15,12 @@ connectDB();
 const app = express();
 
 // 3. Middlewares Globales
-app.use(cors()); // Permite peticiones desde Vercel
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    credentials: true,
+  }),
+); // Permite peticiones desde Vercel
 app.use(morgan("dev")); // Logs de peticiones en consola
 app.use(express.json()); // Permite recibir JSON en el body
 
@@ -31,11 +36,11 @@ app.get("/", (req, res) => {
 // 5. Arranque del servidor
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`
   ==========================================
   ✅ Servidor corriendo en el puerto: ${PORT}
-  🌍 Modo: ${process.env.NODE_ENV || 'development'}
+  🌍 Modo: ${process.env.NODE_ENV || "development"}
   ==========================================
   `);
 });
